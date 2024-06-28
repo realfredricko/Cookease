33889d38f15fbf93869ff66e44e93822c22c931d
realfredricko <ochiengfredrick880@gmail.com>
2024-06-28 14:51:07 +0300

    Updated network module

--- app/src/main/java/com/example/cookease/di/NetworkModule.kt
+++ app/src/main/java/com/example/cookease/di/NetworkModule.kt
@@ -1,12 +1,42 @@
 package com.example.cookease.di
 
+import com.example.cookease.cookease_features.data.remote.CookeaseAPI
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
     //How to provide an instance of OKHTTP Client
 private const val BASE_URL = "https://api.spoonacular.com/"
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15,TimeUnit.SECONDS)
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideCookeaseAPI(retrofit: Retrofit):CookeaseAPI{
+return retrofit.create(CookeaseAPI::class.java)
+    }
 }
